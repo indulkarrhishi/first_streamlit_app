f502f2c84b1c3366ce51ecc55d889900bab5e56a
Rhishikesh Shriram Indulkar <105984584+indulkarrhishi@users.noreply.github.com>
2022-05-23 12:39:32 +0530

    Update streamlit_app.py
    import at top and added one import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
   
 streamlit.header('Breakfast Menu')
@@ -8,7 +13,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,7 +30,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())   # just writes the data to the screen    ----------> line to be removed
 
@@ -38,8 +42,6 @@ streamlit.dataframe(fruityvice_normalized)
 # don't run any thing past here while we troubleshoot
 streamlit.stop()
 
-
-import snowflake.connector
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
